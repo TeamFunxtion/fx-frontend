ff50babd4b3bfb988ca194b3f49040f0f9da9926
usopp <gwee1216@naver.com>
2024-05-10 17:58:32 +0900

    [#35] Style: 배너 이미지 영역

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
-import Image from "next/image";
+import ProductCard from "@/components/products/ProductCard/ProductCard";
+import { API_URL } from "./constants";
+import styles from "./page.module.css"
 
-export default function AppPage() {
+export async function getAllProducts() {
+  const response = await fetch(`${API_URL}/products`);
+  return await response.json();
+}
+
+export default async function AppPage() {
+  const result = await getAllProducts();
+  console.log(result);
+  const products = result.content;
+
   return (
-    <h1>Hello Next</h1>
+    <div>
+      <div className={styles.mainBanner}></div>
+      <ul className={styles.productList}>
+        {
+          products.map((product, index) => (
+            <ProductCard product={product} />
+          ))
+        }
+      </ul>
+    </div>
   );
 }
