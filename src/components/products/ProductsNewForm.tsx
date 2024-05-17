c2a0897af914a3738def75cac14f1fd5c6b75290
usopp <gwee1216@naver.com>
2024-05-17 11:48:08 +0900

    [#57] Fix: 상품등록 react-hook-form 활용 validate 검증 추가

--- src/components/products/ProductsNewForm.tsx
+++ src/components/products/ProductsNewForm.tsx
@@ -4,13 +4,14 @@ import styles from "./ProductsNewForm.module.css"
 import { useForm, SubmitHandler } from "react-hook-form"
 import toast from "react-hot-toast"
 import { useRouter } from "next/navigation"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { categories } from "@/app/constants"
 import { useRecoilValue } from "recoil"
 import { userInfoState } from "@/store/atoms"
 import MRadio, { TRadioGroup } from "../atom/MRadio"
 import axios, { AxiosInstance } from "axios"
 import api, { fileApi } from "@/utils/api"
+import { ErrorMessage } from "@hookform/error-message"
 
 interface FormValues {
 	storeId: string,
@@ -58,6 +59,11 @@ export default function ProductsNewForm({ product }) {
 	const [imgFile, setImgFile] = useState([]);
 
 	const onSubmitHandler: SubmitHandler<FormValues> = async (values) => {
+		if (!id) {
+			router.push("/auth/login");
+			return false;
+		}
+
 		// console.log(values)
 
 		// const fd = new FormData();
@@ -86,7 +92,12 @@ export default function ProductsNewForm({ product }) {
 		// 	})
 
 
-		const requestbody = { ...values, storeId: id, categoryId };
+		const requestbody = {
+			...values,
+			storeId: id,
+			categoryId,
+			productPrice: values.productPrice.replace(/[^0-9]+/g, ""),
+		};
 
 		if (isNew) { // 생성 
 			const res = await api.post('/products', requestbody);
@@ -113,14 +124,14 @@ export default function ProductsNewForm({ product }) {
 	}
 
 	const handleChangeFile = (event: any) => {
-		console.log(imgFile.length);
-		console.log(event.target.files.length);
+		// console.log(imgFile.length);
+		// console.log(event.target.files.length);
 		if (imgFile.length + event.target.files.length > FILE_MAX_COUNT) {
 			toast.error(`사진 첨부는 최대 ${FILE_MAX_COUNT}장까지 가능합니다.`);
 			return false;
 		}
 
-		console.log(event.target.files);
+		// console.log(event.target.files);
 		setImgFile(imgFile => [...imgFile, ...event.target.files]);
 		for (let i = 0; i < event.target.files.length; i++) {
 			if (event.target.files[i]) {
@@ -128,7 +139,7 @@ export default function ProductsNewForm({ product }) {
 				reader.readAsDataURL(event.target.files[i]);
 				reader.onloadend = () => {
 					const base64 = reader.result; // 비트맵 데이터 리턴, 이 데이터를 통해 파일 미리보기가 가능함
-					console.log(base64)
+					// console.log(base64)
 					if (base64) {
 						let base64Sub = base64.toString()
 						setImgBase64(imgBase64 => [...imgBase64, base64Sub]);
@@ -137,7 +148,6 @@ export default function ProductsNewForm({ product }) {
 			}
 		}
 	}
-
 
 	const removeImg = (index) => {
 		const newImgFile = [...imgFile];
@@ -148,13 +158,19 @@ export default function ProductsNewForm({ product }) {
 		setImgBase64(newImgBase64);
 	}
 
+	useEffect(() => {
+		if (!id) {
+			router.push("/auth/login");
+		}
+	}, [])
+
 	return (
 		<section className={styles.section}>
 			<form onSubmit={handleSubmit(onSubmitHandler)}>
 				<ul className={styles.list}>
 					<h2 className={styles.listTitle}>상품 정보</h2>
 					<li className={styles.listRow}>
-						<div className={styles.rowLabel}>상품 이미지({imgFile.length}/{FILE_MAX_COUNT})</div>
+						<div className={styles.rowLabel}>상품 이미지<span className={styles.small}>({imgFile.length}/{FILE_MAX_COUNT})</span></div>
 						<div className={styles.rowContent}>
 							<ul className={styles.images}>
 								<label htmlFor="file">
@@ -175,13 +191,18 @@ export default function ProductsNewForm({ product }) {
 						</div>
 					</li>
 					<li className={styles.listRow}>
-						<div className={styles.rowLabel}>상품명</div>
+						<div className={styles.rowLabel}>상품명<sup className={styles.required}>*</sup><span className={styles.small}>({watch("productTitle").length}/30)</span></div>
 						<div className={styles.rowContent}>
-							<input className={styles.input} {...register("productTitle", { required: true, maxLength: { value: 100, message: '최대 100글자까지 입력이 가능합니다.' } })} type="text" placeholder="상품명" />
+							<input className={styles.input}
+								{...register("productTitle", { required: "필수값 입니다.", maxLength: { value: 30, message: '최대 30글자까지 입력이 가능합니다.' } })}
+								type="text"
+								placeholder="상품명"
+								maxLength={30} />
+							<p className={styles.error}><ErrorMessage errors={errors} name="productTitle" /></p>
 						</div>
 					</li>
 					<li className={styles.listRow}>
-						<div className={styles.rowLabel}>카테고리</div>
+						<div className={styles.rowLabel}>카테고리<sup className={styles.required}>*</sup></div>
 						<div className={styles.rowContent}>
 							<ul className={styles.categoryList}>
 								{
@@ -193,15 +214,30 @@ export default function ProductsNewForm({ product }) {
 						</div>
 					</li>
 					<li className={styles.listRow}>
-						<div className={styles.rowLabel}>가격</div>
+						<div className={styles.rowLabel}>가격<sup className={styles.required}>*</sup><span className={styles.small}>(1억미만)</span></div>
 						<div className={styles.rowContent}>
-							<input className={styles.input} {...register("productPrice", { required: true })} type="text" placeholder="가격" />
+							<input className={styles.input}
+								{...register("productPrice", {
+									required: '필수값 입니다.',
+									pattern: {
+										value: /[0-9]/,
+										message: '숫자만 입력 가능합니다.'
+									},
+								})}
+								type="text"
+								placeholder="가격"
+								maxLength={8} />
+							<p className={styles.error}><ErrorMessage errors={errors} name="productPrice" /></p>
 						</div>
 					</li>
 					<li className={styles.listRow}>
-						<div className={styles.rowLabel}>설명</div>
+						<div className={styles.rowLabel}>설명<sup className={styles.required}>*</sup><span className={styles.small}>({watch("productDesc").length}/1000)</span></div>
 						<div className={styles.rowContent}>
-							<textarea className={styles.textArea} {...register("productDesc", { required: true, maxLength: { value: 1000, message: "최대 1000자까지 입력이 가능합니다." } })} placeholder="브랜드, 모델명, 구매 시기, 하자 유무 등 상품 설명을 최대한 자세히 적어주세요." />
+							<textarea className={styles.textArea}
+								{...register("productDesc", { required: '필수값 입니다.', maxLength: { value: 1000, message: "최대 1000자까지 입력이 가능합니다." } })}
+								placeholder="브랜드, 모델명, 구매 시기, 하자 유무 등 상품 설명을 최대한 자세히 적어주세요."
+								maxLength={1000} />
+							<p className={styles.error}><ErrorMessage errors={errors} name="productDesc" /></p>
 						</div>
 					</li>
 					<li className={styles.listRow}>
@@ -216,9 +252,14 @@ export default function ProductsNewForm({ product }) {
 						</div>
 					</li>
 					<li className={styles.listRow}>
-						<div className={styles.rowLabel}>거래 희망 지역</div>
+						<div className={styles.rowLabel}>거래 희망 지역<span className={styles.small}>({watch("location").length}/15)</span></div>
 						<div className={styles.rowContent}>
-							<input className={styles.input} {...register("location", { required: true, maxLength: { value: 30, message: "최대 30글자까지 입력이 가능합니다." } })} type="text" placeholder="거래 희망 지역" />
+							<input className={styles.input} {...register("location",
+								{ required: false, maxLength: { value: 15, message: "최대 15글자까지 입력이 가능합니다." } })}
+								type="text"
+								placeholder="거래 희망 지역"
+								maxLength={15}
+							/>
 						</div>
 					</li>
 					<li className={styles.listRow}>
@@ -239,9 +280,20 @@ export default function ProductsNewForm({ product }) {
 					salesTypeId !== "SA03" && <ul className={styles.list}>
 						<h2 className={styles.listTitle}>경매 정보</h2>
 						{salesTypeId === "SA01" && <li className={styles.listRow}>
-							<div className={styles.rowLabel}>즉시 구매가</div>
+							<div className={styles.rowLabel}>즉시 구매가<span className={styles.small}>(1억미만)</span></div>
 							<div className={styles.rowContent}>
-								<input className={styles.input} {...register("coolPrice")} type="text" placeholder="즉시 구매가" />
+								<input className={styles.input}
+									{...register("coolPrice", {
+										pattern: {
+											value: /[0-9]/,
+											message: '숫자만 입력 가능합니다.'
+										},
+									})}
+									type="text"
+									placeholder="즉시 구매가"
+									maxLength={8}
+								/>
+								<p className={styles.error}><ErrorMessage errors={errors} name="coolPrice" /></p>
 							</div>
 						</li>}
 						<li className={styles.listRow}>
