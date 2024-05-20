48e9e7d70fa75873e96a3463a4afdc1052897f85
usopp <gwee1216@naver.com>
2024-05-20 18:49:27 +0900

    Feat: 마이페이지 메뉴바 본인일때만 보이게,,

--- src/app/(shop)/shop/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ShopPage() {
-	return (
-		<div>
-			<h1>마이페이지</h1>
-		</div>
-	)
-}
--- src/components/FxTab.tsx
+++ src/components/FxTab.tsx
@@ -2,7 +2,7 @@
 import styles from "@/styles/FxTab.module.css"
 import { useState } from "react"
 
-export default function FxTab({ items, onClick }) {
+export default function FxTab({ items, onClick, fullWidth }) {
 	const [index, setIndex] = useState(0);
 
 	const clickTab = (idx) => {
@@ -11,7 +11,7 @@ export default function FxTab({ items, onClick }) {
 	}
 
 	return (
-		<ul className={styles.tabList}>
+		<ul style={{ display: 'grid', gridTemplateColumns: fullWidth ? `repeat(${items.length}, 1fr)` : 'repeat(auto-fill, minmax(160px, 1fr))' }}>
 			{
 				items && items.map((tab, idx) => (
 					<li className={`${styles.tabItem} ${idx === index && styles.active}`} key={idx} onClick={() => clickTab(idx)}>
