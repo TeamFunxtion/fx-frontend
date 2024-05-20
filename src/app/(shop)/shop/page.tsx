293ed80083711a9281ff975f7d7591e2e8c9e72f
usopp <gwee1216@naver.com>
2024-05-20 14:29:23 +0900

    [#71] Fix: 마이페이지 화면 라우터 수정

--- src/app/(shop)/shop/page.tsx
+++ src/app/(shop)/shop/page.tsx
@@ -1,7 +1,6 @@
 export default function ShopPage() {
 	return (
 		<div>
-
 			<h1>마이페이지</h1>
 		</div>
 	)
--- src/components/shop/shop-menubar.tsx
+++ src/components/shop/shop-menubar.tsx
@@ -3,34 +3,39 @@ import Link from "next/link"
 import styles from "./shop-menubar.module.css"
 import { useRecoilValue } from "recoil"
 import { userInfoState } from "@/store/atoms"
+import { usePathname } from "next/navigation"
 
 export default function ShopMenubar() {
 	const user = useRecoilValue(userInfoState);
 	const baseUrl = `/shop/${user.id}`;
+
+	const pathname = usePathname();
+	console.log(pathname);
+
 	return (
 		<div className={styles.container}>
 			<h1 className={styles.pageTitle}>마이페이지</h1>
 			<ul className={styles.listContainer}>
 				<h3 className={styles.listTitle}>거래 정보</h3>
-				<li><Link href={`${baseUrl}/products`}>내 상품</Link></li>
-				<li><Link href={`${baseUrl}/auctions`}>진행중 경매</Link></li>
-				<li><Link href={`${baseUrl}/bids`}>참여중 경매</Link></li>
-				<li><Link href="/shop">판매 내역</Link></li>
-				<li><Link href="/shop">구매 내역</Link></li>
+				<li className={`${pathname.indexOf('/products') > -1 ? 'active' : ''}`}><Link href={`${baseUrl}/products`}>내 상품</Link></li>
+				<li className={`${pathname.indexOf('/auctions') > -1 ? 'active' : ''}`}><Link href={`${baseUrl}/auctions`}>진행중 경매</Link></li>
+				<li className={`${pathname.indexOf('/bids') > -1 ? 'active' : ''}`}><Link href={`${baseUrl}/bids`}>참여중 경매</Link></li>
+				<li className={`${pathname.indexOf('/sell') > -1 ? 'active' : ''}`}><Link href={`${baseUrl}/sell`}>판매 내역</Link></li>
+				<li className={`${pathname.indexOf('/buy') > -1 ? 'active' : ''}`}><Link href={`${baseUrl}/buy`}>구매 내역</Link></li>
 			</ul>
 
 			<ul className={styles.listContainer}>
 				<h3 className={styles.listTitle}>내 정보</h3>
-				<li><Link href="/shop/profile">회원정보 수정</Link></li>
-				<li><Link href="/shop">관심 상품</Link></li>
-				<li><Link href="/shop/following">팔로잉</Link></li>
-				<li><Link href="/shop/follower">팔로워</Link></li>
-				<li><Link href="/shop">문의 내역</Link></li>
+				<li className={`${pathname.indexOf('/profile') > -1 ? 'active' : ''}`}><Link href={`${baseUrl}/profile`}>회원정보 수정</Link></li>
+				<li className={`${pathname.indexOf('/favorites') > -1 ? 'active' : ''}`}><Link href={`${baseUrl}/favorites`}>관심 상품</Link></li>
+				<li className={`${pathname.indexOf('/following') > -1 ? 'active' : ''}`}><Link href={`${baseUrl}/following`}>팔로잉</Link></li>
+				<li className={`${pathname.indexOf('/follower') > -1 ? 'active' : ''}`}><Link href={`${baseUrl}/follower`}>팔로워</Link></li>
+				<li><Link href="/qna">문의 내역</Link></li>
 			</ul>
 
 			<ul className={styles.listContainer}>
 				<h3 className={styles.listTitle}>결제 정보</h3>
-				<li><Link href="/shop/payments">결제 내역</Link></li>
+				<li className={`${pathname.indexOf('/payments') > -1 ? 'active' : ''}`}><Link href={`${baseUrl}/payments`}>결제 내역</Link></li>
 			</ul>
 		</div >
 	)
