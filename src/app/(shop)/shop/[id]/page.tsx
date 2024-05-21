3751779b4cc5fac9c1c47e69099a9db2bcad303b
usopp <gwee1216@naver.com>
2024-05-21 12:34:31 +0900

    Feat: 판매자 상품 목록 조회 (게스트)

--- src/app/(shop)/shop/[id]/page.tsx
+++ src/app/(shop)/shop/[id]/page.tsx
@@ -3,7 +3,7 @@ import ShopVisit from "@/components/shop/shop-visit";
 export default function ShopPage({ params }: { params: { id: string } }) {
 	return (
 		<div>
-			<ShopVisit />
+			<ShopVisit params={params} />
 		</div>
 	)
 }
