8f5289f4dac18c5d3915e9b3fd216062b2f7ea68
usopp <gwee1216@naver.com>
2024-05-23 14:02:32 +0900

    feat: 오류 페이지 추가

---
+++ src/app/error.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+import ErrorPage from "@/components/ErrorPage"
+
+export default function GlobalError() {
+	return <ErrorPage />
+}
---
+++ src/components/ErrorPage.tsx
@@ -0,0 +1,8 @@
+
+export default function ErrorPage() {
+	return (
+		<div className="error">
+			😅 알 수 없는 오류가 발생했습니다. <br />
+		</div>
+	)
+}
