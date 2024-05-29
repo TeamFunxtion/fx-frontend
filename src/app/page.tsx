c9229e9c8e0675f43add62936cf83428cd260662
usopp <gwee1216@naver.com>
2024-05-29 16:57:29 +0900

    Fix: 홈화면 client단에서 상품목록조회 요청

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import { API_URL } from "./constants";
 import Home from "@/components/Home";
 
 export async function getAllProducts() {
-  const response = await fetch(`${API_URL}/products?size=100`, { cache: 'no-store' });
+  const timestamp = new Date().getTime(); // Generate a unique timestamp
+  const response = await fetch(`${API_URL}/products?size=100&timestamp=${timestamp}`, { cache: 'no-store' });
   return await response.json();
 }
 
 export default async function AppPage() {
-  const result = await getAllProducts();
-  const products = result.content;
+  // const result = await getAllProducts();
   return (
-    <Home products={products} />
+    <Home />
   );
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,8 +2,22 @@
 import SimpleImageSlider from "react-simple-image-slider";
 import ProductCard from "./products/ProductCard/ProductCard"
 import styles from '@/styles/Home.module.css'
+import { useEffect, useState } from "react";
+import api from "@/utils/api";
 
-export default function Home({ products }) {
+export default function Home() {
+	const [list, setList] = useState([]);
+
+	const getProductList = async () => {
+		const result = await api.get(`/products?size=1000`);
+		const { data: { content } } = result;
+		setList(content);
+	}
+
+	useEffect(() => {
+		getProductList();
+	}, []);
+
 	return (
 		<div>
 			<div className={styles.mainBanner}>
@@ -23,7 +37,7 @@ export default function Home({ products }) {
 			</div>
 			<ul className={styles.productList}>
 				{
-					products && products.map((product, index) => (
+					list && list.map((product, index) => (
 						<ProductCard product={product} key={index} />
 					))
 				}
