49b5e158139b6824e273b255bb6f704d95867a27
RyuYeonWoo <dusdn5218534@gmail.com>
2024-05-31 12:42:57 +0900

    [#131] Fix 공지사항 사이드바 현재페이지에 맞춰 파란색으로 변경

--- src/components/etc/etcsidebar.tsx
+++ src/components/etc/etcsidebar.tsx
@@ -1,17 +1,19 @@
 import Link from "next/link";
 import styles from "./etcsidebar.module.css";
+import { usePathname } from "next/navigation";
 
 export default function Etcsidebar() {
+	const pathname = usePathname();
+
+
 
 	return (
 		<aside className={styles.etcAside}>
 			<h3 className={styles.etcNotice}>고객센터</h3>
 			<br />
-
-
-			<li className={styles.etcAsideLi}><Link href="/notice">공지사항</Link></li>
-			<li className={styles.etcAsideLi}><Link href="/faq">자주 묻는 질문</Link></li>
-			<li className={styles.etcAsideLi}><Link href="/qna">1:1문의</Link></li>
+			<li className={pathname.indexOf('/notice') > -1 ? styles.etcAsideSerch : styles.etcAsideLi}><Link href="/notice" >공지사항</Link></li>
+			<li className={pathname.indexOf('/faq') > -1 ? styles.etcAsideSerch : styles.etcAsideLi}><Link href="/faq" >자주 묻는 질문</Link></li>
+			<li className={pathname.indexOf('/qna') > -1 ? styles.etcAsideSerch : styles.etcAsideLi}><Link href="/qna" >1:1문의</Link></li>
 		</aside >
 	)
 
